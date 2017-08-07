b0a0d292bddeca023b602d911321ff45ca7181d5
SchruteFarms23 <romanmullady@gmail.com>
2017-08-07 16:48:29 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,31 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+# binding.pry
+"My name is Roman"
+end
+
+get '/hometown' do
+  "My hometown is Briarwood"
+end
+
+get '/favorite-song' do
+  "My favorite song is Encore"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
